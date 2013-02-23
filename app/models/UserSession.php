7ec483796eec1c51436184cce6dc8ea1882b54f3
baris kanat <bkanat@gmail.com>
2013-02-23 09:06:03 -0500

    modify session_start method

--- app/models/UserSession.php
+++ app/models/UserSession.php
@@ -6,13 +6,13 @@ class UserSession{
     private $current;
     private $user_finder;
     
-    public function user_find($user){
-        
-        $this->user_finder=$user;
-    }
+  
     
-    public function session_start($user){
+    public function session_start($user,$time=null){
+        if(!is_null($time)&&  is_int($time))
+            Config::set("Session.lifetime",$time);
         Session::put($this->sessionname,(int)$user->id);
+        $this->current=$user;
     }
     
     public function session_destroy(){
