57106050a0c507227d8a8ad72be4ba0ae15b334b
baris kanat <bkanat@gmail.com>
2013-03-14 22:09:37 -0400

    work on user photo page

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 
     <!-- Le styles -->
  
--- app/views/photo/index.blade.php
+++ app/views/photo/index.blade.php
@@ -4,6 +4,24 @@
 
 <h1>photo section</h1>
 
+<div id="mainphoto">
+    <div id="photosection">
+        
+        <div id="uinfo">
+            
+            <img src="<?php echo $location.$user->picture; ?>">
+            
+            <span>{{{HTML::route('userpage', $user->username, array('user' => $user->username))}}}</span>
+            
+        </div>
+        
+        <img src="<?php echo $location."b_".$photo->filename ;?>">
+    </div>
+    
+    <div id="photocomment">
+        
+        
+    </div>
 
-<img src="<?php echo $location."b_".$photo->filename ;?>">
+</div>
 @stop
