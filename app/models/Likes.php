3220995a89fe4d7ad6d97561154fd85f905c70ec
baris kanat <bkanat@gmail.com>
2013-03-21 22:29:24 -0400

    first commit

---
+++ app/models/Likes.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class Feed extends Base{
+    
+    protected $user;
+    
+    public function __construct(User $user)
+    {
+        $this->user=$user;
+    }
+    
+    public function index($id)
+    {
+        
+    }
+    
+    
+    public function add($id){
+        
+    }
+    
+    
+    public function remove($id)
+    {
+        
+    }
+    
+   public function lists($id)
+   {
+       
+   }
+}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
